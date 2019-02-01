f8aedf64a6396f7ed2e919b08671f82d7134c43f
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 22:31:34 +0000

    Automatically backed up by Learn

--- lib/shoe.rb
+++ lib/shoe.rb
@@ -13,11 +13,6 @@ class Shoe
     puts "Your shoe is as good as new!"
   end
   
-  def brand=(brand)
-    @brand = brand
-    if BRANDS.include?(brand) == false
-      BRANDS << brand
-    end
-  end
+  
 
 end
