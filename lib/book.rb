c40a09d05e57c63bd1fa303e94e50fc725dd0cbd
John Hayes <johnnyhayes7@gmail.com>
2019-02-01 22:20:45 +0000

    Automatically backed up by Learn

--- lib/book.rb
+++ lib/book.rb
@@ -2,7 +2,7 @@ class Book
   attr_accessor :author, :page_count, 
   attr_reader :title, :genre
   
-  GENERES = []
+  GENRES = []
 
   def initialize(title)
     @title = title
@@ -12,6 +12,8 @@ class Book
     puts "Flipping the page...wow, you read fast!"
   end
   
-  
+  def genre=(genre)
+    @genre = genre
+    GENRES << genre
 
 end
